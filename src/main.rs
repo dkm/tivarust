441fbed1d67031be606def03dfbeea4d7109a466
Marc Poulhiès <dkm@kataplop.net>
2017-02-19 00:08:46 +0100

    initial vector pointing to main: works

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ pub extern fn main() {
     loop{}
 }
 
+// #[lang = "eh_personality"]
+// extern "C" fn eh_personality() {
+// }
+
 #[lang = "panic_fmt"]
 pub extern fn rust_begin_panic(_msg: core::fmt::Arguments,
                                _file: &'static str,
@@ -15,3 +19,14 @@ pub extern fn rust_begin_panic(_msg: core::fmt::Arguments,
 }
 
 
+// extern {
+//     fn _stack_top();
+// }
+
+#[link_section = ".vectors"]
+// #[no_mangle]
+pub static _VECTORS: [unsafe extern fn(); 2] = [
+    // _stack_top,
+    main,
+    main,
+];
