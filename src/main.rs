46a69ba77181c3a0c13e9730a2eb9114d3b8b984
Marc Poulhiès <github@kataplop.net>
2017-02-22 23:11:47 +0100

    try to use bit-banding for enabling gpio periph

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@
 #[no_mangle]
 pub extern fn main() {
 
+    const SYSCTL_RCGBASE: u32 = 0x400fe600;
 
+    const SYSCTL_PERIPH_GPIOF: u32 = 0xf0000805;
+    
     const RCGCGPIO: *mut u32 = (0x400FE000 + 0x608) as *mut u32;
 
     const RCGCGPIO_PORTA: u32 = 0x1;
@@ -30,21 +33,31 @@ pub extern fn main() {
     const GPIO_PORT_F_GPIOIS: *mut u32 = (GPIO_PORT_F + 0x404) as *mut u32;    
     const GPIO_PORT_F_GPIODEN: *mut u32 = (GPIO_PORT_F + 0x51C) as *mut u32;    
 
+
+    
     unsafe {
-        *RCGCGPIO |= RCGCGPIO_PORTF;
-        *GPIO_PORT_F_GPIODIR |= 0x1<<3;
-        *GPIO_PORT_F_GPIODEN |= 0x1<<3;
+        
+        // bit-band access
+        const ENABLE_GPIO_F : *mut u32 =
+            (((SYSCTL_RCGBASE + ((SYSCTL_PERIPH_GPIOF & 0xff00) >> 8)) & 0xF0000000) | 0x02000000 |
+             (((SYSCTL_RCGBASE + ((SYSCTL_PERIPH_GPIOF & 0xff00) >> 8)) & 0x000FFFFF) << 5) |
+             ((SYSCTL_PERIPH_GPIOF & 0xff) << 2)) as *mut u32;
+
+        *ENABLE_GPIO_F = 1;
+        // HWREGBITW(SYSCTL_RCGCBASE + ((ui32Peripheral & 0xff00) >> 8),
+        //           ui32Peripheral & 0xff) = 1;
+        
+
+        // *RCGCGPIO |= RCGCGPIO_PORTF;
+        // *GPIO_PORT_F_GPIODIR |= 0x1<<3;
+        // *GPIO_PORT_F_GPIODEN |= 0x1<<3;
         
         loop {
             *GPIO_PORT_F_GPIODATA |= 0x1<<3;
             // *GPIO_PORT_F_GPIODATA &= !(0x1<<3);
 
         }
-
-
     }
-    
-    loop{}
 }
 
 // #[lang = "eh_personality"]
@@ -59,15 +72,30 @@ pub extern fn rust_begin_panic(_msg: core::fmt::Arguments,
 }
 
 
-// extern {
-//     fn _stack_top();
-// }
+extern {
+    fn _stack_top();
+}
+
+pub extern fn error_handler() {
+    loop{}
+}
+
 
 #[link_section = ".vectors"]
 // #[no_mangle]
-pub static _VECTORS: [unsafe extern fn(); 2] = [
-    // _stack_top,
+pub static _VECTORS: [unsafe extern fn(); 13] = [
+    _stack_top,
     main,
-    main,
+    error_handler, // NMI
+    error_handler, // Hard fault
+    error_handler, // Mem Management
+    error_handler, // bus fault
+    error_handler, // usage fault
+    error_handler, // NONE
+    error_handler, // NONE
+    error_handler, // NONE
+    error_handler, // NONE
+    error_handler, // SVcall
+    error_handler, // Debug monitor
 ];
 
