0130d9f7f658cc02cfba8c8a703663d84b46402e
Marc Poulhiès <github@kataplop.net>
2017-02-23 22:28:35 +0100

    add some toy macros

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,52 @@
 #![no_std]
 #![no_main]
 
+macro_rules! bitband {
+    ($x:expr, $b:expr) => {{
+        let _x = $x;
+        ((_x & 0xF0000000) | 0x02000000 |           
+        ((_x & 0x000FFFFF) << 5) | (($b) << 2))
+    }}
+}
 
+
+macro_rules! hwreg {
+    ($x:expr, $t:ty) => {{
+        let _r : *mut $t = ($x) as *mut $t;
+        *_r;
+    }};
+
+    ($x:expr, $t:ty, $v:expr) => {{
+        let _r : *mut $t = ($x) as *mut $t;
+        *_r = $v;
+    }}
+    
+}
+
+const SYSCTL_RCGBASE: u32 = 0x400fe600;
+const SYSCTL_PERIPH_GPIOF: u32 = 0xf0000805;
+const GPIO_LOCK_KEY: u32 = 0x4C4F434B;
+const GPIO_LOCK_R_OFF: u32 = 0x520;
+const GPIO_PORTF_BASE: u32 = 0x40025000;  // GPIO Port F
+
+pub fn sys_ctl_peripheral_enable(periph : u32) {
+    unsafe {
+        let enable_periph : *mut u32 =
+            bitband!((SYSCTL_RCGBASE + ((periph & 0xff00) >> 8)),
+                     (periph & 0xff)) as *mut u32;
+        *enable_periph = 1;
+    }
+}
+
+#[no_mangle]
+pub unsafe fn __aeabi_unwind_cpp_pr0() -> ()
+{
+    loop {}
+}
 
 #[no_mangle]
 pub extern fn main() {
 
-    const SYSCTL_RCGBASE: u32 = 0x400fe600;
-
-    const SYSCTL_PERIPH_GPIOF: u32 = 0xf0000805;
-    
     const RCGCGPIO: *mut u32 = (0x400FE000 + 0x608) as *mut u32;
 
     const RCGCGPIO_PORTA: u32 = 0x1;
@@ -38,12 +75,14 @@ pub extern fn main() {
     unsafe {
         
         // bit-band access
-        const ENABLE_GPIO_F : *mut u32 =
-            (((SYSCTL_RCGBASE + ((SYSCTL_PERIPH_GPIOF & 0xff00) >> 8)) & 0xF0000000) | 0x02000000 |
-             (((SYSCTL_RCGBASE + ((SYSCTL_PERIPH_GPIOF & 0xff00) >> 8)) & 0x000FFFFF) << 5) |
-             ((SYSCTL_PERIPH_GPIOF & 0xff) << 2)) as *mut u32;
 
-        *ENABLE_GPIO_F = 1;
+        // const ENABLE_GPIO_F : *mut u32 =
+        //     (((SYSCTL_RCGBASE + ((SYSCTL_PERIPH_GPIOF & 0xff00) >> 8)) & 0xF0000000) | 0x02000000 |
+        //      (((SYSCTL_RCGBASE + ((SYSCTL_PERIPH_GPIOF & 0xff00) >> 8)) & 0x000FFFFF) << 5) |
+        //      ((SYSCTL_PERIPH_GPIOF & 0xff) << 2)) as *mut u32;
+        sys_ctl_peripheral_enable(SYSCTL_PERIPH_GPIOF);
+        hwreg!(GPIO_PORTF_BASE + GPIO_LOCK_R_OFF, u32, GPIO_LOCK_KEY);
+        hwreg!(GPIO_PORTF_BASE + GPIO_LOCK_R_OFF, u32, 0);
         // HWREGBITW(SYSCTL_RCGCBASE + ((ui32Peripheral & 0xff00) >> 8),
         //           ui32Peripheral & 0xff) = 1;
         
@@ -65,6 +104,7 @@ pub extern fn main() {
 // }
 
 #[lang = "panic_fmt"]
+#[no_mangle]
 pub extern fn rust_begin_panic(_msg: core::fmt::Arguments,
                                _file: &'static str,
                                _line: u32) -> ! {
